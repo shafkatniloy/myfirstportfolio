0d5c68d280dbb8e40ae0f21d059f66ac786b76e0
Safkat Islam Niloy <164136080+shafkatniloy@users.noreply.github.com>
2025-11-24 02:39:36 +0600

    photo card

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -13,8 +13,8 @@
 */
 
 // smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
+$(document).ready(function () {
+    $(".navbar .nav-link").on('click', function (event) {
 
         if (this.hash !== "") {
 
@@ -24,15 +24,46 @@ $(document).ready(function(){
 
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 700, function(){
+            }, 700, function () {
                 window.location.hash = hash;
             });
-        } 
+        }
     });
 });
 
 // navbar toggle
-$('#nav-toggle').click(function(){
+$('#nav-toggle').click(function () {
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
+});
+
+// About Me Slideshow
+$(document).ready(function () {
+    const images = [
+        'assets/imgs/m4.jpg',
+        'assets/imgs/m1.jpg',
+        'assets/imgs/m3.png'
+    ];
+
+    let currentIndex = 0;
+    const $aboutImg = $('#aboutImage');
+    const $dots = $('.dot');
+
+    function updateDots(index) {
+        $dots.removeClass('active');
+        $dots.eq(index).addClass('active');
+    }
+
+    if ($aboutImg.length) {
+        setInterval(function () {
+            $aboutImg.addClass('fade-out');
+
+            setTimeout(function () {
+                currentIndex = (currentIndex + 1) % images.length;
+                $aboutImg.attr('src', images[currentIndex]);
+                updateDots(currentIndex);
+                $aboutImg.removeClass('fade-out');
+            }, 500); // Wait for fade out transition
+        }, 3000); // Change every 3 seconds
+    }
 });
